0ec4c2ef03b515661377f5c04189e98ba5491fe6
Kucing7676 <faizalaliapriyono@gmail.com>
2023-07-04 17:59:13 +0700

    add enemy hit

---
+++ Treasure Hunter/Assets/Scripts/Enemy/EnemyHit.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class EnemyHit : MonoBehaviour
+{
+    [SerializeField] private float damagePoint;
+    [SerializeField] private float hitTime;
+    private float _hitTimer = 0;
+    private bool _canHit = true;
+    public Health player;
+    
+
+    private void Update() {
+        _hitTimer += Time.deltaTime;
+        Debug.Log(_hitTimer);
+
+        if (_hitTimer > hitTime)
+        {
+            _canHit = true;
+        } else
+        {
+        _canHit = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D Collide)
+    {
+        if(Collide.name == "Player")
+        {
+            if (_canHit == true)
+            {
+                Debug.Log ("You got Hit");
+                player.TakeDamage(damagePoint);
+                _hitTimer = 0;
+            }
+        }
+    }
+}
