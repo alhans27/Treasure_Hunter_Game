68eeb820c7a47d45d4a9e673a5b9d6134f6af670
hanif26 <55867304+hanif26@users.noreply.github.com>
2023-07-06 21:23:43 +0700

    Scriptable Object: inisiasi awal Inventory SO

---
+++ Treasure Hunter/Assets/Scripts/UI/Backpack/InventorySO.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class InventorySO : ScriptableObject
+{
+    [SerializeField]
+    private List<ItemInventory> inventoryItems;
+
+    [field: SerializeField]
+    public int Size { get; set; } = 5;
+
+    public void Initialize()
+    {
+        inventoryItems = new List<ItemInventory>();
+        for (int i = 0; i < Size; i++)
+        {
+            inventoryItems.Add(ItemInventory.GetEmptyItem());
+        }
+    }
+
+    public void AddItem(ItemSO item, int quantity)
+    {
+        for (int i = 0; i < inventoryItems.Count; i++)
+        {
+            if (inventoryItems[i].IsEmpty)
+            {
+                inventoryItems[i] = new ItemInventory
+                {
+                    item = item,
+                    quantity = quantity,
+                };
+            }
+        }
+    }
+
+    public Dictionary<int, ItemInventory> GetCurrentInventoryState()
+    {
+        Dictionary<int, ItemInventory> returnValue = new Dictionary<int, ItemInventory>();
+
+        for (int i = 0; i < inventoryItems.Count; i++)
+        {
+            if (inventoryItems[i].IsEmpty)
+                continue;
+            returnValue[i] = inventoryItems[i];
+        }
+        return returnValue;
+    }
+}
+
+[Serializable]
+public struct ItemInventory
+{
+    public int quantity;
+    public ItemSO item;
+    public bool IsEmpty => item == null;
+
+    public ItemInventory ChangeQuantity(int newQuantity)
+    {
+        return new ItemInventory
+        {
+            item = this.item,
+            quantity = newQuantity,
+        };
+    }
+
+    public static ItemInventory GetEmptyItem()
+        => new ItemInventory
+        {
+            item = null,
+            quantity = 0,
+        };
+}
